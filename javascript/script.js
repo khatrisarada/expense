5c26826dc7db647e991a18394e66030bf833c817
khatrisarada <saradakhattri07@gmail.com>
2025-01-28 17:04:16 +0545

    Update changes in Expense

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+// What are some commonly used methods for manipulating strings in JavaScript? Provide examples demonstrating their usage.
+//Concatination
+console.log("=>Concatination");
+let str1 ="Good";
+let str2 ="Morning";
+let result = str1.concat(" ",str2);
+console.log(result);
+
+//string Lenght
+console.log("=>String Length");
+let text ="Sarada";
+let Length= text.length;
+console.log(Length);
+
+//
